3d70100d185a0304075f2207043f57475b37ebee
MiKyung Lee <58964324+mlee03@users.noreply.github.com>
2025-03-07 09:19:49 -0500

    add create_xgrid, constants, and grid_utils (#6)
    * add create_xgrid, constants, and grid_utils
    
    * fix space
    
    ---------
    
    Co-authored-by: mlee03 <Mikyung.Lee@lscamd50-d.gfdl.noaa.gov>

---
+++ c_horiz_interp/c_horiz_interp.F90
@@ -0,0 +1,53 @@
+module c_horiz_interp_mod
+
+  use iso_c_binding
+  implicit none
+
+  private
+
+  public :: cFMS_create_xgrid_2dx2d_order1
+  public :: cFMS_get_maxxgrid
+  
+contains
+
+  !cFMS_create_xgrid_2dx2d_order1
+  function cFMS_create_xgrid_2dx2d_order1(nlon_in, nlat_in, nlon_out, nlat_out, lon_in, lat_in, lon_out, &
+       lat_out, mask_in, maxxgrid, i_in, j_in, i_out, j_out, xgrid_area) bind(C, name="cFMS_create_xgrid_2dx2d_order1")
+
+    integer, intent(in) :: nlon_in
+    integer, intent(in) :: nlat_in
+    integer, intent(in) :: nlon_out
+    integer, intent(in) :: nlat_out
+    real(c_double), intent(in) :: lon_in((nlon_in+1)*(nlat_in+1))
+    real(c_double), intent(in) :: lat_in((nlon_in+1)*(nlat_in+1))
+    real(c_double), intent(in) :: lon_out((nlon_out+1)*(nlat_out+1))
+    real(c_double), intent(in) :: lat_out((nlon_out+1)*(nlat_out+1))
+    real(c_double), intent(in) :: mask_in(nlon_in*nlat_in)
+    integer, intent(in) :: maxxgrid
+    real(c_double), intent(out) :: i_in(maxxgrid)
+    real(c_double), intent(out) :: j_in(maxxgrid)
+    real(c_double), intent(out) :: i_out(maxxgrid)
+    real(c_double), intent(out) :: j_out(maxxgrid)
+    real(c_double), intent(out) :: xgrid_area(maxxgrid)
+
+    integer create_xgrid_2dx2d_order1
+    integer cFMS_create_xgrid_2dx2d_order1
+    
+    cFMS_create_xgrid_2dx2d_order1 = create_xgrid_2dx2d_order1(nlon_in, nlat_in, nlon_out, nlat_out, lon_in, lat_in, &
+         lon_out, lat_out, mask_in, i_in, j_in, i_out, j_out, xgrid_area)
+    
+  end function cFMS_create_xgrid_2dx2d_order1
+
+  !cFMS_get_maxxgrid
+  function cFMS_get_maxxgrid() bind(C, name="cFMS_get_maxxgrid")
+
+    implicit none
+    integer :: get_maxxgrid
+    integer :: cFMS_get_maxxgrid
+
+    cFMS_get_maxxgrid = get_maxxgrid()
+
+  end function cFMS_get_maxxgrid
+
+  
+end module c_horiz_interp_mod
