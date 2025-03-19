14d5db599db22788696b75852d02a50f29662a56
mlee03 <Mikyung.Lee@lscamd50-d.gfdl.noaa.gov>
2025-03-19 10:19:58 -0400

    make optional arguments null pointers

--- c_data_override/c_data_override.F90
+++ c_data_override/c_data_override.F90
@@ -1,6 +1,6 @@
 module c_data_override_mod
 
-  use FMS, only: FmsMppDomain2D, FATAL, fms_mpp_error
+  use FMS, only: FmsMppDomain2D, FmsMppDomainUG, FATAL, fms_mpp_error
   use FMS, only: fms_data_override_init
 
   use c_fms_mod, only : cFMS_get_domain_from_id
@@ -11,7 +11,7 @@ module c_data_override_mod
   private
   public :: cFMS_data_override_init
 
-  integer, public,  bind(C, name="CFLOAT_MODE")  :: CFLOAT_MODE = c_float
+  integer, public, bind(C, name="CFLOAT_MODE")  :: CFLOAT_MODE = c_float
   integer, public, bind(C, name="CDOUBLE_MODE") :: CDOUBLE_MODE = c_double
   
 contains
@@ -27,17 +27,32 @@ contains
     integer, intent(in), optional :: land_domainUG_id
     integer, intent(in), optional :: mode
 
-    type(FmsMppDomain2D), pointer :: atm_domain
+    type(FmsMppDomain2D), pointer :: atm_domain 
+    type(FmsMppDomain2D), pointer :: ocn_domain
+    type(FmsMppDomain2D), pointer :: ice_domain
+    type(FmsMppDomain2D), pointer :: land_domain
+    type(FmsMppDomainUG), pointer :: land_domainUG
 
-    if(present(ocn_domain_id))    call fms_mpp_error(FATAL, "ocn_domain will be implemented in the near future")
-    if(present(ice_domain_id))    call fms_mpp_error(FATAL, "ice_domain will be implemented in the near future")
-    if(present(land_domain_id))   call fms_mpp_error(FATAL, "land_domain will be implemented in the near future")
-    if(present(land_domainUG_id)) call fms_mpp_error(FATAL, "land unstructured domain will be implemented in the near future")
-    
+    atm_domain => NULL()
+    ocn_domain => NULL()
+    ice_domain => NULL()
+    land_domain => NULL()
+    land_domainUG => NULL()
+
+    !NULL pointers are interpreted as not present optional arguments 
+    !https://fortran-lang.discourse.group/t/an-unallocated-variable-passed-as-an-argument-is-not-present/1724/3
     if(present(atm_domain_id)) atm_domain => cFMS_get_domain_from_id(atm_domain_id)
-
-    call fms_data_override_init(atm_domain_in = atm_domain,&
-                                mode = mode)    
+    if(present(ocn_domain_id)) ocn_domain => cFMS_get_domain_from_id(ocn_domain_id)
+    if(present(ice_domain_id)) ice_domain => cFMS_get_domain_from_id(ice_domain_id)
+    if(present(land_domain_id)) land_domain => cFMS_get_domain_from_id(land_domain_id)
+    if(present(land_domainUG_id)) call fms_mpp_error(FATAL, "unstructured domain is currently not implemented")
+    
+    call fms_data_override_init(atm_domain_in = atm_domain,   &
+                                ocean_domain_in = ocn_domain, &
+                                ice_domain_in = ice_domain,   &
+                                land_domain_in = land_domain,  &
+                                land_domainUG_in = land_domainUG, &
+                                mode = mode)
     
   end subroutine cFMS_data_override_init
   
