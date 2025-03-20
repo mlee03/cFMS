10acfa0d1984d86d03ec6f688bf2ddd0d3b42d3e
mlee03 <Mikyung.Lee@lscamd50-d.gfdl.noaa.gov>
2025-03-20 12:58:39 -0400

    add array_t_pointer

--- c_fms_utils/c_fms_utils.F90
+++ c_fms_utils/c_fms_utils.F90
@@ -5,6 +5,7 @@ module c_fms_utils_mod
 
   private
   public :: cFMS_pointer_to_array
+  public :: cFMS_array_to_pointer
   
   interface cFMS_pointer_to_array
      module procedure cFMS_pointer_to_array_2d_int
@@ -20,9 +21,15 @@ module c_fms_utils_mod
      module procedure cFMS_pointer_to_array_4d_cdouble
      module procedure cFMS_pointer_to_array_5d_cdouble
   end interface cFMS_pointer_to_array
+
+  interface cFMS_array_to_pointer
+     module procedure cFMS_array_to_pointer_2d_int
+     module procedure cFMS_array_to_pointer_3d_int
+  end interface cFMS_array_to_pointer
   
 contains
 
 #include "pointer_to_array.fh"
+#include "array_to_pointer.fh"
   
 end module c_fms_utils_mod
